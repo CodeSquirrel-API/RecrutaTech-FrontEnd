dcb8b377721a066abc674a253750bc87b87041a9
Ana Silva <43580298+AnaChristina@users.noreply.github.com>
2023-09-15 07:21:44 -0300

    atualizando icons

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 import { RouterLink, RouterView } from 'vue-router'
 
 import HelloWorld from './components/HelloWorld.vue'
-
+import Btn_Salvar from './components/Btn_Salvar.vue'
 </script>
 
  
@@ -13,13 +13,14 @@ import HelloWorld from './components/HelloWorld.vue'
 
   <header>
 
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
  
 
     <div class="wrapper">
 
       <HelloWorld msg="You did it!" />
+      <Btn_Salvar />
 
  
 
