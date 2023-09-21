1da4ef1f9d51bb0074e586416904f879e6932975
Debora Faria <deeborafaria@gmail.com>
2023-09-21 12:04:44 -0300

    Create multiline text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import Cha from '../components/Cha.vue'
 
 </script>
-
-<template>
+  <template>
     <div>
       <h1 class="title">Descrição da Vaga</h1>
       <label for="descricao" class="input-label">Digite o título do cargo:</label>
@@ -22,7 +20,21 @@ import Cha from '../components/Cha.vue'
       <div class="button-container">
         <button class="custom-button clear-button">Limpar</button>
         <button class="custom-button save-button">Gerar CHA</button>
+      </div>
 
+      <!-- Linha cinza abaixo dos botões -->
+      <hr class="line" />
+
+      <!-- Título "CHA" -->
+      <h2 class="cha-title">CHA - Conhecimentos, Habilidades e Atitudes</h2>
+
+      <!-- Campo de texto multilinea -->
+      <textarea v-model="chaText" class="cha-textarea"></textarea>
+
+      <!-- Botões Editar e Buscar -->
+      <div class="button-container">
+        <button class="custom-button edit-button">Editar</button>
+        <button class="custom-button search-button">Buscar</button>
       </div>
     </div>
   </template>
@@ -51,22 +63,22 @@ import Cha from '../components/Cha.vue'
     margin-right: 25px;
     margin-top: 6px; 
   }
-  
+
   .custom-input:focus {
     border-color: #007bff;
   }
-  
+
   .nivel-container {
     margin-left: 25px;
     margin-top: 30px; 
   }
-  
+
   .span-nivel {
     color: #ffffff; 
     font-size: 20px; 
     margin-bottom: 8px; 
   }
-  
+
   .select-option {
     width: 30%;
     border: 1px solid #ccc;
@@ -75,11 +87,11 @@ import Cha from '../components/Cha.vue'
     transition: border-color 0.2s;
     margin-top: 5px;
   }
-  
+
   .select-option:focus {
     border-color: #007bff;
   }
-  
+
   /* Estilos dos botões */
   .button-container {
     display: flex;
@@ -87,7 +99,7 @@ import Cha from '../components/Cha.vue'
     margin-top: 20px;
     margin-right: 55px;
   }
-  
+
   .custom-button {
     padding: 10px 20px;
     border: none;
@@ -97,19 +109,67 @@ import Cha from '../components/Cha.vue'
     margin-left: 10px;
     transition: background-color 0.2s;
   }
-  
+
   .clear-button {
     background-color: #6666;
     color: #fff;
   }
-  
+
   .save-button {
     background-color: #5D5DFF;
     color: #fff;
   }
-  
+
   .custom-button:hover {
     filter: brightness(0.9); /* Escurece a cor ao passar o mouse */
   }
-  </style>
+
+  .line {
+  border: none;
+  border-top: 1px solid #999898;
+  margin-top: 10px; /* Espaçamento acima da linha */
+  margin-bottom: 10px; /* Espaçamento abaixo da linha */
+}
+
+/* Estilos para o título "CHA" */
+.cha-title {
+  font-size: 22px;
+  margin-top: 30px; 
+  margin-left: 18px;
+}
+
+/* Estilos para o campo de texto multilinear */
+.cha-textarea {
+  width: 100%;
+  height: 200px; 
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  outline: none;
+  margin-top: 10px; 
+  margin-bottom: 10px; 
+}
+
+/* Estilos para os botões "Editar" e "Buscar" */
+.edit-button,
+.search-button {
+  background-color: #5D5DFF; 
+  color: #fff; 
+  border: 1px solid #5D5DFF; 
+  border-radius: 5px; 
+  padding: 10px 20px; 
+  margin-right: 2px; 
+  cursor: pointer;
+  outline: none;
+  transition: background-color 0.3s, color 0.3s;
+}
+
+/* Estilos de hover para os botões "Editar" e "Buscar" */
+.edit-button:hover,
+.search-button:hover {
+  background-color: #4455cc;
+  border-color: #4455cc; 
+}
+
+</style>
   
