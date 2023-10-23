e2ab7737de9cd6cd7097ddead1a874f837fb9be6
Carlos Henrique Benício Costa <ique.benicio@hotmail.com>
2023-10-23 17:09:19 -0300

    informações do email e login
    adicionando a função de ao logar vai ser mandado um codigo ao email registrado  e vai abrir um popup para inserir o codigo nele.

---
+++ src/service/api.tsx
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: "https://api-recrutatech.onrender.com"
+}) 
+const baseURL = axios.defaults.baseURL = "https://api-recrutatech.onrender.com"
+
+export default baseURL; api;
--- src/views/TheLogin.vue
+++ src/views/TheLogin.vue
@@ -8,11 +8,11 @@
 
 
       <h2 class="titulo ">Entrar</h2>
-      <form @submit.prevent="login">
+      <form @submit.prevent="Codigo">
 
         <div class="">
-          <label for="username" class="font" >E-mail:</label>
-          <input type="text" class="campoInput" id="username" v-model="username" placeholder="exemplo@gmail.com"/>
+          <label for="email" class="font" >E-mail:</label>
+          <input type="text" class="campoInput" id="email" v-model="email" placeholder="exemplo@gmail.com"/>
         </div>
 
         <div class="">
@@ -27,10 +27,18 @@
         </div>       
         
         <div class="center">
-          <button @click="entrar" class="entrar">Entrar</button>
-          <RouterLink to="/Home"></RouterLink>
-        </div>
-
+            <button class="open-popup-button entrar" @click="Codigo, showPopupNow()">Entrar</button>
+            <div class="custom-popup" v-if="showPopup">
+              <div class="popup-content">
+                <p class="popup-message">{{ popupMessage }}</p>
+                <br>
+                <input type="number" v-model="code">
+                <br>
+                <button class="close-popup-button" @click="VerificarCodigo">Login</button>
+                <button class="close-popup-button" @click="closePopup">Fechar</button>
+              </div>
+            </div>
+          </div>
 
         <div class="center">
           <label for="text" class="font">Não tem uma conta?
@@ -43,6 +51,39 @@
   </template>
 
 <style scoped>
+
+.custom-popup {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #33363A;
+  border: 1px solid #ccc;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+  padding: 20px;
+  border-radius: 5px;
+  z-index: 1000;
+}
+.popup-content {
+  text-align: center;
+}
+.popup-message {
+  font-size: 18px;
+  color: #FFFFFF;
+}
+.close-popup-button {
+  margin: 30px 30px 30px 30px;
+  border-radius: 10px;
+  width: 133px;
+  height: 40px;
+  background-color: #5D5DFF;
+  color: white;
+  font-size: 20px;
+}
+.close-popup-button:hover {
+  background-color: #2980b9;
+}
+
 .checkbox{
   display:flex;
 }
@@ -116,15 +157,63 @@ label {
 </style>
   
   <script lang="ts">
+import baseURL from '../service/api';
+import axios from 'axios';
+
   export default {
     data() {
       return {
+        code: '',
+        showPopup: false,
+        email:'',
         username: '',
         password: '',
         manterConectado: false,
+        popupMessage:'seu codigo será enviado em 5 minutos para o seu e-mail: ***@***.com, verifique o seu codigo para prosseguir com o seu login!'
       };
     },
     methods: {
+      async Codigo () {
+        try{
+         const response = await axios.post(`${baseURL}/email/send-code`, {
+          "email": this.email,
+        })
+        console.log(response.data)
+        console.log("codigo enviado!", this.email)
+        }
+        catch (error) {
+          console.error('error')
+          
+        }
+      },
+      async VerificarCodigo () {
+        try{
+         const responseCheck = await axios.post(`${baseURL}/email/check-code`, {
+          "email": this.email,
+          "code": this.code,
+        })
+        console.log(responseCheck.data);
+        console.log(this.email);
+
+        if (responseCheck.data.valid) {
+          this.$router.push('/home');
+        } else {
+          alert("Codigo incorreto!!");
+          }
+        }
+        catch (error) {
+          console.error(error);
+          
+        }
+          
+      },
+      showPopupNow() {
+        this.showPopup = true;
+      },
+      closePopup() {
+        this.showPopup = false;
+      },
+
       login() {
         // Aqui você pode implementar a lógica de autenticação, como fazer uma requisição para um servidor.
         // Por simplicidade, vamos apenas imprimir os valores do nome de usuário e senha por agora.
@@ -133,7 +222,7 @@ label {
       },
       entrar() {
       // Use o método de roteamento do Vue Router para redirecionar para a rota desejada
-      this.$router.push('/')
+      this.$router.push('/home')
       },
     },
   };
