90c6beb1a90716c1b5ccc54e58abb4b4819168dc
Ana Silva <43580298+AnaChristina@users.noreply.github.com>
2023-11-04 09:59:57 -0300

    lista de candidatos de acordo com profissao e nivel

--- src/components/Candidatos.vue
+++ src/components/Candidatos.vue
@@ -1,118 +1,220 @@
 <template>
-    <div class="container" style="color: white">
-      <div>
-        <h1>Buscar candidatos</h1>
-        <p>Para encontrar um candidato, selecione o cargo e nível de atuação profissional:</p>
+  <div class="container" style="color: white">
+    <div>
+      <h1>Buscar candidatos</h1>
+      <p>Para encontrar um candidato, selecione o cargo e nível de atuação profissional:</p>
+    </div>
+
+    <div class="select select-btn">
+      <select v-model="selectedCargo" class="select-option txt-select" @change="getNivel(selectedCargo)">
+        <option v-for="(item, index) in candidatesProfessions" :key="index" :value="item" class="select-option txt-select">
+          {{ item }}
+        </option>
+      </select>
+
+      <select v-model="nivel" name="experience" class="select-option txt-select">
+        <option v-for="(experience, index) in positionsExperience" :key="index" :value="experience">
+          {{ experience }}
+        </option>
+      </select>
+      <button @click="buscarCandidatosPorFiltro" class="btn-buscar" >Buscar</button>
+    </div>
+
+    
+
+    <div v-for="(candidato, index) in candidatosFiltrados" :key="index" class="candidato-card">
+
+      <div class="info">
+        <span class="candidato">{{ candidato.name }}</span>
+        <button class="visualizar-button" @click="visualizarCandidato(candidato), visualizar =! visualizar">Visualizar</button>
       </div>
-  
-      <div class="select">
-        <select v-model="selectedCargo" class="select-option txt-select" @change="getNivel(selectedCargo)">
-          <option v-for="(item, index) in candidatesProfessions" :key="index" :value="item" class="select-option txt-select">
-            {{ item }}
-          </option>
-        </select>
-  
-        <select v-model="nivel" name="experience" class="select-option txt-select">
-          <option v-for="(experience, index) in positionsExperience" :key="index" :value="experience">
-            {{ experience }}
-          </option>
-        </select>
-      </div>
-  
-      <div class="listaCandidatos">
-        <button @click="getByProfessionAndExperience(selectedCargo, nivel)" style="background-color: blueviolet;">Buscar candidatos</button>
-      </div>
+
+      
+      <div v-if="visualizar">
+        <div class="candidato-info" v-if="candidatoSelecionado === candidato">
+          <p class="">Informações do Candidato</p>
+          <p><strong>Nome:</strong> {{ candidato.name }}</p>
+          <p><strong>E-mail:</strong> {{ candidato.email }}</p>
+
+          <hr>
+
+          <p style="color:rgb(231, 0, 0);">Conhecimentos</p>
+          <ul>
+            <li v-for="(conhecimento, index) in candidato.knowledgeList" :key="index">
+              <p> {{ conhecimento.name }}</p>
+              <!-- <p><strong>Descrição:</strong> {{ conhecimento.description }}</p> -->
+
+            </li>
+          </ul>
+
+          <hr>
+
+          <p style="color:rgb(231, 0, 0);">Habilidades</p>
+          <ul>
+            <li v-for="(habilidade, index) in candidato.skillsList" :key="index">
+              <p><strong></strong> {{ habilidade.name }}</p>
+              <p><strong></strong> {{ habilidade.description }}</p>
+            </li>
+          </ul>
+
+          <hr>
+
+          <p style="color:rgb(231, 0, 0);">Atitudes</p>
+          <ul>
+            <li v-for="(atitude, index) in candidato.attitudeList" :key="index">
+              <p> {{ atitude.name }}</p>
+              <!-- <p><strong>Descrição:</strong> {{ atitude.description }}</p> -->
+
+            </li>
+          </ul>
+
+        </div>
+    </div>
+  </div>
+
     </div>
 </template>
 
-  
-  
-  <script lang="ts">
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        candidates: [],
-        selectedCargo: '',
-        nivel: '',
-        candidatesCurrentProfession: [],
-        ProfessionExperience: [],
-        positionsExperience: [],
-        positions: [],
-        positionsName: [],
-        candidatesProfession: [],
-        ProfessionName: [],
-        candidatesProfessions: [],
-      };
-    },
-    methods: {
-      async getCandidates() {
-        try {
-          const response = await axios.get('/candidates/getAll');
-          this.candidates = response.data;
-          this.candidatesProfessions = [...new Set(this.candidates.map((cand) => cand.currentProfession))];
-          console.log(this.candidatesProfessions);
-        } catch (error) {
-          console.error('Erro na requisição:', error);
-        }
-      },
-  
-      async getByProfessionAndExperience(selectedCargo, nivel) {
-        try {
-          // Aqui você pode usar 'selectedCargo' e 'nivel' nos parâmetros da sua requisição
-          const response = await axios.get(`/candidates/byProfessionAnaExperience?cargo=${selectedCargo}&nivel=${nivel}`);
-          this.ProfessionExperience = response.data;
-          console.log(this.ProfessionExperience);
-          this.ProfessionName = [...new Set(this.ProfessionExperience.map(profession => profession.currentProfession))];
-          
-        } catch (error) {
-          console.error('Erro na requisição:', error);
-        }
-      },
-  
-      async getPositions() {
-        try {
-          const response = await axios.get('/position/getAll');
-          this.positions = response.data;
-          this.positionsName = [...new Set(this.positions.map((pos) => pos.name))];
-        } catch (error) {
-          console.error('Erro na requisição de posições:', error);
-        }
-      },
-  
-      getNivel(selectedCargo) {
-        // Filtra os candidatos com o cargo selecionado
-        const filteredCandidates = this.candidates.filter(cand => cand.currentProfession === selectedCargo);
+<script lang="ts">
+import axios from 'axios';
 
-        // Mapeia os níveis de experiência dos candidatos filtrados
-        this.positionsExperience = [...new Set(filteredCandidates.map(cand => cand.experiences))];
-        console.log(this.positionsExperience);
-        },
+export default {
+  data() {
+    return {
+      candidates: [],
+      selectedCargo: '',
+      nivel: '',
+      candidatesProfessions: [],
+      positionsExperience: [],
+      candidatosFiltrados: [], 
+      candidatoSelecionado: null,
+      visualizar: false,
+    };
+  },
+  methods: {
+    async getCandidates() {
+      try {
+        const response = await axios.get('/candidates/getAll');
+        this.candidates = response.data;
+        this.candidatesProfessions = [...new Set(this.candidates.map((cand) => cand.currentProfession))];
+      } catch (error) {
+        console.error('Erro na requisição:', error);
+      }
+    },
+
+    async getPositions() {
+      try {
+        const response = await axios.get('/position/getAll');
+        this.positionsExperience = [...new Set(response.data)];
+      } catch (error) {
+        console.error('Erro na requisição de posições:', error);
+      }
+    },
+
+    getNivel(selectedCargo) {
+      const filteredCandidates = this.candidates.filter(cand => cand.currentProfession === selectedCargo);
+      this.positionsExperience = [...new Set(filteredCandidates.map(cand => cand.experiences))];
+    },
 
 
+    buscarCandidatosPorFiltro() {
+      this.candidatosFiltrados = this.candidates.filter(
+        (cand) => cand.currentProfession === this.selectedCargo && cand.experiences === this.nivel
+      );
     },
-  
-    beforeMount() {
-      this.getCandidates();
+
+    visualizarCandidato(candidato) {
+      this.candidatoSelecionado = candidato;
     },
-  };
-  </script>
-  
-  <style scoped>
-  .select-option {
-    width: 30%;
-    border: 1px solid #ccc;
-    border-radius: 10px;
-    outline: none;
-    transition: border-color 0.2s;
-    margin-top: 5px;
-    height: 35px;
-    margin-left: 5px;
-  }
-  
-  .select-option:focus {
-    border-color: #007bff;
-  }
-  </style>
-  
+  },
+
+  beforeMount() {
+    this.getCandidates();
+    this.getPositions();
+  },
+};
+</script>
+
+<style scoped>
+.select-option {
+  width: 30%;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  outline: none;
+  transition: border-color 0.2s;
+  margin-top: 5px;
+  height: 35px;
+  margin-left: 5px;
+}
+
+.select-option:focus {
+  border-color: #007bff;
+}
+
+.visualizar-button {
+	background-color: #5D5DFF;
+	color: white;
+	margin-bottom: 1vh;
+	padding: 8px 16px;
+	display: inline-block;
+	cursor: pointer;
+	border-radius: 5px;
+	box-shadow: 0 5px 15px rgb(0, 0, 0, 0.5);
+	transition: all 0.5s ease;
+}
+.visualizar-button:hover {
+	background-color: #5858ee;
+	transform: translateY(-3px);
+}
+
+.info {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+}
+
+.candidato-card {
+	background: #0e1011;
+	padding: 16px;
+	margin: 5px 2px;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	border-radius: 5px;
+	/* border: none; */
+	align-items: center;
+	border-bottom: solid 1px #ffffff4d;
+  flex: 1;
+}
+
+.candidato-info{
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
+}
+.btn-buscar{
+  background-color: #5D5DFF;
+	color: white;
+  margin-left:3vh;
+	margin-bottom: 1vh;
+	padding: 8px 16px;
+	display: inline-block;
+	cursor: pointer;
+	border-radius: 5px;
+	box-shadow: 0 5px 15px rgb(0, 0, 0, 0.5);
+	transition: all 0.5s ease;
+}
+
+.select-btn {
+  height: 65px;
+  padding: 0 20px;
+  color: #fff;
+  font-size: 22px;
+  background: #0e1011;
+  border-radius: 7px;
+  justify-content: space-between;
+  box-shadow: 0 10px 25px rgba(0, 0, 0, 0.1);
+}
+</style>
