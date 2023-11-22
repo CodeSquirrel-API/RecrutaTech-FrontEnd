1f698a0aa959bf02e9c5dc89297f7d1203f8a3f1
Lucas Rafael Andrade Barroso <lukinhahs777@gmail.com>
2023-11-22 09:33:43 -0300

    Ord Alfbtc Logo btn

--- src/views/BancoCha.vue
+++ src/views/BancoCha.vue
@@ -21,7 +21,7 @@ export default {
       showPopup3: false,
       popupMessage2: ('Busca feita com sucesso'),
       popupMessage3: ('Salvo com sucesso!'),
-      carregando:false,
+      carregando: false,
       loading: '<svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="40px" height="40px" viewBox="0 0 40 40" enable-background="new 0 0 40 40" xml:space="preserve"> <path opacity="0.2" fill="#5D5DFF" d="M20.201,5.169c-8.254,0-14.946,6.692-14.946,14.946c0,8.255,6.692,14.946,14.946,14.946s14.946-6.691,14.946-14.946C35.146,11.861,28.455,5.169,20.201,5.169z M20.201,31.749c-6.425,0-11.634-5.208-11.634-11.634c0-6.425,5.209-11.634,11.634-11.634c6.425,0,11.633,5.209,11.633,11.634C31.834,26.541,26.626,31.749,20.201,31.749z"/> <path fill="#5D5DFF" d="M26.013,10.047l1.654-2.866c-2.198-1.272-4.743-2.012-7.466-2.012h0v3.312h0C22.32,8.481,24.301,9.057,26.013,10.047z"> <animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 20 20" to="360 20 20" dur="0.5s" repeatCount="indefinite"/></path></svg>',
     };
   },
@@ -55,7 +55,7 @@ export default {
       this.buscaRealizada = true; // Marca que uma busca foi realizada
       this.carregando = false;
     },
-       
+
 
     async salvarCha() {
       if (this.cargo && this.nivel) {
@@ -142,6 +142,13 @@ export default {
     Sidebar, Header,
   },
 
+  computed: {
+    sortedPositions() {
+      const lowercasePositions = this.positionsName.map(item => item.toUpperCase());
+      return lowercasePositions.slice().sort();
+    },
+  },
+
   watch: {
     cargo(newValue, oldValue) {
       if (this.buscaRealizada && newValue !== oldValue) {
@@ -161,89 +168,86 @@ export default {
 
 
 <template>
-    <Sidebar></Sidebar>
-    <Header></Header>
-    <div class="page-content">
-      <div class="bancocha">
-        <h1 class="title">Banco de CHA</h1>
+  <Sidebar></Sidebar>
+  <Header></Header>
+  <div class="page-content">
+    <div class="bancocha">
+      <h1 class="title">Banco de CHA</h1>
 
-        <p class="descricao">Selecione o Cargo e Nível abaixo para obter os CHA's que foram salvos.</p>
+      <p class="descricao">Selecione o Cargo e Nível abaixo para obter os CHA's que foram salvos.</p>
 
-        
-        <div class="Cargo">
-          <div>
-            <label for="descricao" class="input-label">Selecione o cargo:</label>
-          </div>
-          
-          <select v-model="cargo" class="select-option txt-select" @change="getNivel(cargo)">
-              <option
-                v-for="(item, index) in positionsName" :key="index"
-                v-bind:value="item"
-                class="select-option txt-select">
-                {{ item }}
-              </option>
-            </select>
+
+      <div class="Cargo">
+        <div>
+          <label for="descricao" class="input-label">Selecione o cargo:</label>
         </div>
-        
-        <div class="nivel-container">
-          <div>
-            <label class="span-nivel">Selecione o nível de atuação profissional:</label>
-          </div>
-          
-          <select v-model="nivel" name="experience" class="select-option txt-select">
-            <option v-for="(experience, index) in positionsExperience" :key="index" v-bind:value="experience">{{ experience }}</option>
-          </select>
+
+        <select v-model="cargo" class="select-option txt-select" @change="getNivel(cargo)">
+          <option v-for="(item, index) in sortedPositions" :key="index" v-bind:value="item"
+            class="select-option txt-select">
+            {{ item }}
+          </option>
+        </select>
+      </div>
+
+      <div class="nivel-container">
+        <div>
+          <label class="span-nivel">Selecione o nível de atuação profissional:</label>
         </div>
-    
-        <!-- Botões  -->
-        <div class="button-container">
-          <div>
-            <button class="custom-button clear-button" @click="LimparCampos()">Limpar</button>
-          </div>
-          <div>
-              <button class="custom-button save-button" @click="BuscarCha()">Buscar</button>
-          </div>
-        </div>
+
+        <select v-model="nivel" name="experience" class="select-option txt-select">
+          <option v-for="(experience, index) in positionsExperience" :key="index" v-bind:value="experience">{{ experience
+          }}</option>
+        </select>
       </div>
 
-      <!-- Linha cinza abaixo dos botões -->
-      <hr class="line" />
-      <p class="descricao">Fique a vontade para editar os campos, a edição só sera salvar quando clicar em salvar.</p>
-      <!-- Título "CHA" -->
-
-      <h2 class="cha-title"> Conhecimento </h2>
-      <div class="textarea-container">
-      <div v-if="carregando" v-html="loading" class="loading-icon"></div>
-        <textarea v-model="conhecimento" class="cha-textarea"></textarea>
-      </div>
-
-      <h2 class="cha-title"> Habilidade </h2>
-      <div class="textarea-container">
-      <div v-if="carregando" v-html="loading" class="loading-icon"></div>
-        <textarea v-model="habilidade" class="cha-textarea"></textarea>
-      </div>
-
-      <h2 class="cha-title"> Atitude </h2>
-      <div class="textarea-container">
-      <div v-if="carregando" v-html="loading" class="loading-icon"></div>
-        <textarea v-model="atitude" class="cha-textarea"></textarea>
-      </div>
-
+      <!-- Botões  -->
       <div class="button-container">
-        <button class="custom-button search-button" @click="salvarCha">Salvar</button>
-          <div class="custom-popup" v-if="showPopup3">
-              <div class="popup-content">
-                <p class="popup-message">{{ popupMessage3 }}</p>
-                <button class="close-popup-button" @click="closePopup3">Fechar</button>
-              </div>
-          </div>
+        <div>
+          <button class="custom-button clear-button" @click="LimparCampos()">Limpar</button>
+        </div>
+        <div>
+          <button class="custom-button save-button" @click="BuscarCha()">Buscar</button>
+        </div>
       </div>
     </div>
-   
+
+    <!-- Linha cinza abaixo dos botões -->
+    <hr class="line" />
+    <p class="descricao">Fique a vontade para editar os campos, a edição só sera salvar quando clicar em salvar.</p>
+    <!-- Título "CHA" -->
+
+    <h2 class="cha-title"> Conhecimento </h2>
+    <div class="textarea-container">
+      <div v-if="carregando" v-html="loading" class="loading-icon"></div>
+      <textarea v-model="conhecimento" class="cha-textarea"></textarea>
+    </div>
+
+    <h2 class="cha-title"> Habilidade </h2>
+    <div class="textarea-container">
+      <div v-if="carregando" v-html="loading" class="loading-icon"></div>
+      <textarea v-model="habilidade" class="cha-textarea"></textarea>
+    </div>
+
+    <h2 class="cha-title"> Atitude </h2>
+    <div class="textarea-container">
+      <div v-if="carregando" v-html="loading" class="loading-icon"></div>
+      <textarea v-model="atitude" class="cha-textarea"></textarea>
+    </div>
+
+    <div class="button-container">
+      <button class="custom-button search-button" @click="salvarCha">Salvar</button>
+      <div class="custom-popup" v-if="showPopup3">
+        <div class="popup-content">
+          <p class="popup-message">{{ popupMessage3 }}</p>
+          <button class="close-popup-button" @click="closePopup3">Fechar</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
 .textarea-container {
   position: relative;
 }
@@ -256,16 +260,18 @@ export default {
   transform: translate(-50%, -50%);
   /* Adicione outros estilos para o ícone de carregamento, como largura, altura etc. */
 }
-.descricao{
+
+.descricao {
   color: #999898;
   margin: 1vh 0vh 4vh 4vh;
   font-size: 17px;
 }
 
 .page-content {
-  margin-left: 18%; /* Use o mesmo valor da largura do menu */
+  margin-left: 18%;
+  /* Use o mesmo valor da largura do menu */
   width: 100%;
-  height: 100vh; 
+  height: 100vh;
 }
 
 .custom-popup {
@@ -280,13 +286,16 @@ export default {
   border-radius: 5px;
   z-index: 1000;
 }
+
 .popup-content {
   text-align: center;
 }
+
 .popup-message {
   font-size: 18px;
   color: #FFFFFF;
 }
+
 .close-popup-button {
   margin: 30px 30px 30px 30px;
   border-radius: 10px;
@@ -296,110 +305,117 @@ export default {
   color: white;
   font-size: 20px;
 }
+
 .close-popup-button:hover {
   background-color: #2980b9;
 }
 
-  .bancocha{
-    width: 100%;
-  }
-  .title {
-    color: rgb(255, 255, 255); 
-    font-size: 30px; 
-    font-weight: bold; 
-    margin-left: 14px;
-    margin-top: 20px;
-  }
-  .input-label {
-    color: #ffffff; 
-    font-size: 20px; 
-    margin-bottom: 8px;
-  }
-  .custom-input {
-    width: 90%;
-    padding: 8px;
-    border: 1px solid #ccc;
-    border-radius: 10px;
-    outline: none;
-    transition: border-color 0.2s;
-    margin-left: 25px;
-    margin-right: 25px;
-    margin-top: 6px; 
-  }
+.bancocha {
+  width: 100%;
+}
 
-  .custom-input:focus {
-    border-color: #007bff;
-  }
+.title {
+  color: rgb(255, 255, 255);
+  font-size: 30px;
+  font-weight: bold;
+  margin-left: 14px;
+  margin-top: 20px;
+}
 
-  .nivel-container {
-    margin-top: 5%;
-    margin-left: 1%;
-  }
+.input-label {
+  color: #ffffff;
+  font-size: 20px;
+  margin-bottom: 8px;
+}
 
-  .span-nivel {
-    color: #ffffff; 
-    font-size: 20px; 
-    margin-bottom: 8px; 
-  }
+.custom-input {
+  width: 90%;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  outline: none;
+  transition: border-color 0.2s;
+  margin-left: 25px;
+  margin-right: 25px;
+  margin-top: 6px;
+}
 
-  .select-option {
-    width: 30%;
-    border: 1px solid #ccc;
-    border-radius: 10px;
-    outline: none;
-    transition: border-color 0.2s;
-    margin-top: 5px;
-    height:35px ;
-    margin-left: 5px;
-  }
+.custom-input:focus {
+  border-color: #007bff;
+}
 
-  .select-option:focus {
-    border-color: #007bff;
-  }
+.nivel-container {
+  margin-top: 5%;
+  margin-left: 1%;
+}
 
-  /* Estilos dos botões */
-  .button-container {
-    display: flex;
-    justify-content: flex-end;
-    margin-top: 20px;
-    margin-right: 55px;
-  }
+.span-nivel {
+  color: #ffffff;
+  font-size: 20px;
+  margin-bottom: 8px;
+}
 
-  .custom-button {
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    font-size: 16px;
-    cursor: pointer;
-    margin-left: 10px;
-    transition: background-color 0.2s;
-  }
+.select-option {
+  width: 30%;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  outline: none;
+  transition: border-color 0.2s;
+  margin-top: 5px;
+  height: 35px;
+  margin-left: 5px;
+}
 
-  .clear-button {
-    background-color: #6666;
-    color: #fff;
-  }
+.select-option:focus {
+  border-color: #007bff;
+}
 
-  .save-button {
-    background-color: #5D5DFF;
-    color: #fff;
-  }
+/* Estilos dos botões */
+.button-container {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 20px;
+  margin-right: 55px;
+}
 
-  .custom-button:hover {
-    filter: brightness(0.9); /* Escurece a cor ao passar o mouse */
-  }
+.custom-button {
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  font-size: 16px;
+  cursor: pointer;
+  margin-left: 10px;
+  transition: background-color 0.2s;
+}
 
-  .line {
+.clear-button {
+  background-color: #6666;
+  color: #fff;
+}
+
+.save-button {
+  background-color: #5D5DFF;
+  color: #fff;
+}
+
+.custom-button:hover {
+  filter: brightness(0.9);
+  /* Escurece a cor ao passar o mouse */
+}
+
+.line {
   border: none;
   border-top: 1px solid #999898;
-  margin-top: 10px; /* Espaçamento acima da linha */
-  margin-bottom: 10px; /* Espaçamento abaixo da linha */
+  margin-top: 10px;
+  /* Espaçamento acima da linha */
+  margin-bottom: 10px;
+  /* Espaçamento abaixo da linha */
 }
 
 /* Estilos para o título "CHA" */
 .cha-title {
   font-size: 22px;
-  margin-top: 30px; 
+  margin-top: 30px;
   color: #fff;
   margin-left: 25px;
 }
@@ -423,26 +439,26 @@ export default {
 /* Estilos para os botões "Editar" e "Buscar" */
 .edit-button,
 .search-button {
-  background-color: #5D5DFF; 
-  color: #fff; 
-  border: 1px solid #5D5DFF; 
-  border-radius: 5px; 
-  padding: 10px 20px; 
-  margin-right: 15px; 
+  background-color: #5D5DFF;
+  color: #fff;
+  border: 1px solid #5D5DFF;
+  border-radius: 5px;
+  padding: 10px 20px;
+  margin-right: 15px;
   cursor: pointer;
   outline: none;
   transition: background-color 0.3s, color 0.3s;
-  box-shadow: 0 5px 10px rgb(0,0,0,0.3);
+  box-shadow: 0 5px 10px rgb(0, 0, 0, 0.3);
 }
 
 /* Estilos de hover para os botões "Editar" e "Buscar" */
 .edit-button:hover,
 .search-button:hover {
   background-color: #4455cc;
-  border-color: #4455cc; 
+  border-color: #4455cc;
 }
 
-.Cargo{
+.Cargo {
   margin-left: 1%;
 }
 </style>
